f1277bf0593558e175f17d937d71a7f4e8c6fa91
iamnaymur <nrtusher1@gmail.com>
2023-08-18 09:31:55 +0600

    php variables and declaration

---
+++ README.md
@@ -0,0 +1,8 @@
+Php = Hypertext preprocessor and it runs on the server.
+Php is not case sensitive, though it is case sensitive for the variable declarations.
+The (.) dot is used for concationation purposes
+
+Variable rules -
+1 . Start; with $
+2 . can not use special character in it.
+3 . Can not use numbers at the beginning.
--- index.php
+++ index.php
@@ -1,14 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <?php 
-    $food ='banana';
 
-    echo 'I love to eat ' . $food; ?>
+<body>
+    <?php
+    $food = 'banana';
+    $location = "narayanganj, bangladesh";
+    $age = 20;
+    $name = 'naymur rahman ';
+    echo 'Hello my name is ' . $name . 'and my age is ' . $age . '. ' . '<br>' . 'I live in ' . $location . '. My favorite fruit is ' . $food . '.';
+
+
+    // numbers 
+    $x = 10;
+    $y = 20;
+    echo '<br>' . 'The total of the numbers is ' . $x + $y;
+    print '<br>' . 'The total of the numbers is ' . $x + $y;
+    echo ('<br>' . 'The total of the numbers is ' . $x * $y . '<br>');
+
+
+    print $food . '<br>';
+    $foods = print('drinks');
+    echo $foods . '<br>';
+
+    $checkFloat = 30.52;
+    $religionIslam = true;
+
+    $friends = array('shafin', 'topu', 'jisan', 'nahin', 'sharmin', 'akibe', 'miraj');
+    $checkNull = null;
+
+    var_dump($name);
+    var_dump($age);
+    var_dump($checkFloat);
+    var_dump($religionIslam);
+    var_dump($friends);
+    var_dump($checkNull);
+
+    ?>
+
+
 </body>
+
 </html>
